0dacdc2babfb7f2253e702920fa2c2012f4da72b
Ayush Singh <ayush2003sing@gmail.com>
2025-07-03 11:04:24 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,44 +6,21 @@ resource "aws_key_pair" "deployer" {
 resource "aws_default_vpc" "default" {}
 
 resource "aws_security_group" "allow_user_to_connect" {
-  name        = "allow_tls"
-  description = "Allow inbound traffic for SSH, HTTP, HTTPS"
-  vpc_id      = aws_default_vpc.default.id
+  name   = "allow_tls"
+  vpc_id = aws_default_vpc.default.id
 
   ingress {
-    description = "Allow SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
-    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "mysecurity"
   }
 }
 
