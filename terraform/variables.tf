0441690001277fc17309ea261a781e6a04ce7ad9
Ayush Singh <ayush2003sing@gmail.com>
2025-07-03 10:57:15 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,17 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
+  type        = string
   default     = "eu-north-1"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-042b4708b1d05f512"
+  type        = string
+  default     = "ami-042b4708b1d05f512"  # Verify this AMI exists in eu-north-1
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
+  type        = string
   default     = "t2.large"
 }
