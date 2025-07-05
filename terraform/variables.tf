5e61f7ab711f284319a546848c4814cc09f22794
Ayush Singh <ayush2003sing@gmail.com>
2025-07-05 12:46:46 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,5 +13,5 @@ variable "ami_id" {
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
   type        = string
-  default     = "t3.xlarge"
+  default     = "t2.large"
 }
